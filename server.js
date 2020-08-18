c2ae2b34281a290b6d5c631a05099d3382b52e11
Ayush Salik <asalik13@gmail.com>
2020-08-19 01:03:31 +0530

    hmm

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const isProduction = process.env.NODE_ENV === "production";
 const PORT = process.env.PORT || 8000;
 const host = process.env.HOST;
 const app = express();
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, "./build")));
 app.get("*", (req, res) => {
-  return res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  return res.sendFile(path.resolve(__dirname, "./build", "index.html"));
 });
 app.listen(PORT, () => console.log("Server running on port " + PORT));
